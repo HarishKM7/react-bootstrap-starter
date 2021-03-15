0df44d0cea31fd48bf9699c4a2166936cef53e23
Harish KM <harishkm@harishkm.in>
2021-03-15 16:42:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <Container className="text-center">
+      <h1>
+        <a href="/react-bootstrap-starter/">
+          React Bootstrap Starter
+        </a>
+      </h1>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@forevolve/bootstrap-dark/dist/css/bootstrap-dark.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
